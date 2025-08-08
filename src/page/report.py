1184336e51e1849a33d75502619f88e39fea8732
Camilo Andrade <andradecamilo226@gmail.com>
2025-08-08 13:40:45 -0500

    bugs de dataframe con datos

--- src/page/report.py
+++ src/page/report.py
@@ -23,31 +23,36 @@ def reporte_general(datos):
     hoja = st.selectbox("Elige una hoja de calculo: ", df_names.sheet_names)
 
     if excel_archivo and hoja:
-        df = pd.read_excel(
-            os.path.join(ruta, excel_archivo),
-            sheet_name=hoja,
-            engine="calamine",
-        )
-        try:
-            df.columns = df.columns.str.lower()
 
-            estandares = {
-                n: int(df[n][0]) for n in df.columns if "min" in n or "max" in n
-            }
-            for col in df.columns:
-                tipos = df[col].map(type).unique()
-                if len(tipos) > 1:
-                    df[col] = df[col].astype(str)
-            st.dataframe(df)
-            st.subheader("Parametros Estandar")
-            st.write(
-                "Orden de parametros: peso, elongaciones a lo ancho y luego alogaciones  a lo largo"
+        try:
+            df = pd.read_excel(
+                os.path.join(ruta, excel_archivo),
+                sheet_name=hoja,
+                engine="calamine",
             )
 
-            if len(estandares) > 0:
-                st.dataframe(pd.DataFrame([estandares]), hide_index=True)
+            if not df.empty:
+                df.columns = df.columns.str.lower()
+
+                estandares = {
+                    n: int(df[n][0]) for n in df.columns if "min" in n or "max" in n
+                }
+                for col in df.columns:
+                    tipos = df[col].map(type).unique()
+                    if len(tipos) > 1:
+                        df[col] = df[col].astype(str)
+                st.dataframe(df)
+                st.subheader("Parametros Estandar")
+                st.write(
+                    "Orden de parametros: peso, elongaciones a lo ancho y luego alogaciones  a lo largo"
+                )
+
+                if len(estandares) > 0:
+                    st.dataframe(pd.DataFrame([estandares]), hide_index=True)
+                else:
+                    st.info("No estan los estandares en la excel toca bucar por now!")
             else:
-                st.info("No estan los estandares en la excel toca bucar por now!")
+                st.warning("Elige otra hoja de cálculo")
         except:
             print("Error en  la creación de la tabla")
 
@@ -55,9 +60,10 @@ def reporte_general(datos):
             ["Gráficas", "Análisis  Clásico", "Crear Reporte"]
         )
 
-        with tab_grafica:
-            grafica_filtro(df)
-        with tab_resum:
-            analisis(df=df)
-        with crear_reporte:
-            reporte_crear(df)
+        if not df.empty:
+            with tab_grafica:
+                grafica_filtro(df)
+            with tab_resum:
+                analisis(df)
+            with crear_reporte:
+                reporte_crear(df)
