6e4cf724ab979128863885c15f2db0b336674d78
Camilo Andrade <andradecamilo226@gmail.com>
2025-08-08 08:00:53 -0500

    columnas adicionales

--- src/components/reporte_create.py
+++ src/components/reporte_create.py
@@ -12,29 +12,36 @@ def reporte_crear(df: DataFrame):
     )
     lista_principal = columnas_coincidencias(df)
 
-    lista_columnas = []
-    # lista_columnas = st.multiselect(
-    #     "Seleccionar columnas adicionales",
-    #     df.columns,
-    #     key="columnas elegir",
-    #     help="Elegir columnas aparte de las que ya estan y se agregaran al final de la tabla",
-    #     placeholder="Ejemplo: mallas...",
-    # )
-
-    lista_complet = (
-        lista_principal + lista_columnas
-        if lista_columnas and len(lista_columnas) > 0
-        else lista_principal
+    lista_columnas = st.multiselect(
+        "Seleccionar columnas adicionales",
+        df.columns,
+        key="columnas elegir",
+        help="Elegir columnas aparte de las que ya estan y se agregaran al final de la tabla",
+        placeholder="Ejemplo: mallas...",
     )
 
+    lista_complet = lista_principal
+
     if lista_complet:
-        df_filtrado = df[lista_complet].copy()
+        df_filtrado = df.copy()
         df_filtrado["min_peso"] = min_peso
         df_filtrado["max_peso"] = max_peso
         df_filtrado["min_elog_ancho"] = min_ancho
         df_filtrado["max_elog_ancho"] = max_ancho
         df_filtrado["min_elog_largo"] = min_largo
         df_filtrado["max_elog_largo"] = max_largo
+
+        columnas_limtes = [
+            "min_peso",
+            "max_peso",
+            "min_elog_ancho",
+            "max_elog_ancho",
+            "min_elog_largo",
+            "max_elog_largo",
+        ]
+
+        lista_complet = lista_principal + columnas_limtes + lista_columnas
+        df_filtrado = df_filtrado[lista_complet]
         st.dataframe(df_filtrado, hide_index=True)
 
         # generar excel
